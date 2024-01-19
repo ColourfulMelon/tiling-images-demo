2b1347b9de3b345324e9789c5126ed326c871e8d
Jordyn Nitch <NTCJOR001@myuct.ac.za>
2024-01-19 12:17:16 +0200

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: ['cdn.pixabay.com', 'picsum.photos'],
+	}
+};
 
 export default nextConfig;
